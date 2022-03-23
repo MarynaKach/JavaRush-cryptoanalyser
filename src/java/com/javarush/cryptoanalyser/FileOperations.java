e3f6ae96345c5fb820c36414fed61069e0aec1fe
Maryna Kachalova <masha.kach80@gmail.com>
2022-03-23 19:05:07 +0300

    Implementation of algorithm of FileOperations, add methods readFile and writeFile

--- src/java/com/javarush/cryptoanalyser/FileOperations.java
+++ src/java/com/javarush/cryptoanalyser/FileOperations.java
@@ -1,4 +1,26 @@
 package com.javarush.cryptoanalyser;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileOperations {
+    public static char[] readFile(String passToFile) throws IOException {
+        FileInputStream in = new FileInputStream(passToFile);
+        FileReader input = new FileReader(passToFile);
+        char[] encryptedText = new char[in.readAllBytes().length];
+        input.read(encryptedText);
+
+        return encryptedText;
+    }
+
+    public static String writeFile(char[] encryptedText, String pathDestinationFile) throws IOException {
+        FileWriter out = new FileWriter(pathDestinationFile);
+        out.write(encryptedText);
+        out.flush();
+
+        return pathDestinationFile;
+    }
 }
+
