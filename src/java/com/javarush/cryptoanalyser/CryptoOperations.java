bd74fab38bc45b1cd2d5bc0b6021b7c53fa333fe
Maryna Kachalova <masha.kach80@gmail.com>
2022-03-23 19:12:31 +0300

    Implementation of algorithm of CryptoOperations, add methods encryptionText and decryptionText

--- src/java/com/javarush/cryptoanalyser/CryptoOperations.java
+++ src/java/com/javarush/cryptoanalyser/CryptoOperations.java
@@ -1,4 +1,40 @@
 package com.javarush.cryptoanalyser;
 
 public class CryptoOperations {
+    public static char[] encryptionText(char[] encryptedText, int cryptoKey) {
+        char[] resultOfEncryption = new char[encryptedText.length];
+        for (int i = 0; i < encryptedText.length; i++) {
+            encryptedText[i] = (encryptedText[i] + "").toLowerCase().charAt(0);
+            for (int k = 0; k < Main.ALPHABET.length; k++) {
+                if (encryptedText[i] == Main.ALPHABET[k]) {
+                    int resultIndex = k + cryptoKey;
+                    if (resultIndex > Main.ALPHABET.length - 1) {
+                        resultOfEncryption[i] = Main.ALPHABET[resultIndex - Main.ALPHABET.length];
+                    } else {
+                        resultOfEncryption[i] = Main.ALPHABET[resultIndex];
+                    }
+                }
+            }
+        }
+        return resultOfEncryption;
+    }
+
+    public static char[] decryptionText(char[] encryptedText, int cryptoKey) {
+        char[] resultDecryption = new char[encryptedText.length];
+        for (int i = 0; i < encryptedText.length; i++) {
+            encryptedText[i] = (encryptedText[i] + "").toLowerCase().charAt(0);
+            for (int k = 0; k < Main.ALPHABET.length; k++) {
+                if (encryptedText[i] == Main.ALPHABET[k]) {
+                    int resultIndex = k - cryptoKey;
+                    if (resultIndex < 0) {
+                        resultDecryption[i] = Main.ALPHABET[Main.ALPHABET.length + resultIndex];
+                    } else {
+                        resultDecryption[i] = Main.ALPHABET[resultIndex];
+                    }
+                }
+            }
+        }
+        return resultDecryption;
+    }
 }
+
